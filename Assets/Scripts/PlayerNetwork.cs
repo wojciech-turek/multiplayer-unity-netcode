5e8813ffb2db9eac13161bcf84ab025aacf8d97b
wojciech-turek <contact@wojciechturek.com>
2022-10-27 20:38:09 +0200

    Add movement script

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Player : NetworkBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
+    [SerializeField] float playerMovementSpeed = 3f;
     private void Update()
     {
+        if (!IsOwner) return;
+        Vector3 moveDir = new Vector3(0, 0, 0);
 
+        if (Input.GetKey(KeyCode.W)) moveDir.z += 1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z -= 1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x -= 1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x += 1f;
+
+        transform.position += moveDir * playerMovementSpeed * Time.deltaTime;
     }
 }
